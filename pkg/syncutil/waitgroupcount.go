8bb2a3763d3f033f34e143f1cbb88555fe287817
Vojtěch Bargl <bargl.vojtech@gmail.com>
2023-10-25 11:40:02 +0200

    feat(syncutil): added WaitGroupCount

---
+++ pkg/syncutil/waitgroupcount.go
@@ -0,0 +1,25 @@
+package syncutil
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type WaitGroupCount struct {
+	sync.WaitGroup
+	count int64
+}
+
+func (wg *WaitGroupCount) Add(delta int) {
+	atomic.AddInt64(&wg.count, int64(delta))
+	wg.WaitGroup.Add(delta)
+}
+
+func (wg *WaitGroupCount) Done() {
+	atomic.AddInt64(&wg.count, -1)
+	wg.WaitGroup.Done()
+}
+
+func (wg *WaitGroupCount) Count() uint {
+	return uint(atomic.LoadInt64(&wg.count))
+}
